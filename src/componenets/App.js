ba90ae395c04e7017afc92436d8ad3f06f2befab
RolyatVo <taylor.vo@wsu.edu>
2022-07-09 15:54:02 -0700

    Added nav bar

---
+++ src/componenets/App.js
@@ -0,0 +1,14 @@
+// import './App.css';
+
+import NavBar from './NavBar/NavBar';
+import '../App.css';
+
+function App() {
+  return (
+    <div className='App'>
+          <NavBar></NavBar> 
+      </div>
+  );
+}
+
+export default App;
