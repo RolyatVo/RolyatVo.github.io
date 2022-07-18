159ccf5b36a50a5e6d221147448f0b71120c1588
RolyatVo <taylor.vo@wsu.edu>
2022-07-18 15:07:57 -0700

    Finished Projects page

--- src/componenets/Projects/Projects.js
+++ src/componenets/Projects/Projects.js
@@ -5,12 +5,23 @@ import Cimage from "../../imgs/cimage.png"
 import Final320 from "../../imgs/bg2.png"
 import LotsofYou from "../../imgs/ThereArelotsofyou.png"
 import Nomadicunit from "../../imgs/unit.JPG"
-
+import TypeAnimation from "react-type-animation";
 
 function Projects (props) { 
     return (
         <div>
-            <div className="flex justify-center text-4xl font-bold text-white pt-[3%] ">Most Recent Works</div>
+            <div className="About flex justify-center pt-[2%] z-[1] backdrop-blur-3xl bg-[#21272c]/40" >
+                <TypeAnimation
+                    sequence={[
+                        'Most Recent Works', 2000,
+                    ]}
+                    wrapper="p"
+                    repeat={1}
+                    cursor={true}
+                    className="font-bold text-8xl text-center text-slate-200"
+                    />
+                </div>
+                <h1 className="flex justify-center text-slate-200 text-2xl pt-2 backdrop-blur-3xl bg-[#21272c]/40">Some of the projects I've worked on</h1>
             <div className="flex justify-center pt-[2%] pb-[10%]">
                 <div className="grid grid-cols-3 gap-[5rem]">
                     <PCard 
@@ -18,14 +29,14 @@ function Projects (props) {
                         title="Nomadic Unit" 
                         isProject={true}
                         src={Nomadicunit}
-                        desc="Worked closely with Mircosoft Airband team to develop data aquisition software for a remote command center for rural emgergency services. Uses Azure to store all data collected from sensors and to remotely control the unit. There is a website app that utilizes APIs from CosmosDB by Azure."
+                        desc="Worked closely with Mircosoft Airband team to develop data aquisition software for a remote command center for rural emgergency services. Uses Azure to store all data collected from sensors and to remotely control the unit. There is a full-stack website app that utilizes APIs from CosmosDB by Azure."
                         />
                     <PCard 
                         link="https://github.com/ehlong/CXM" 
                         title="Customer Experience Management"
                         isProject={false}
                         src={CXMimage}
-                        desc="A desktop application that is built with ElectronJS."
+                        desc="Developed a full-stack windows application that uses machine learning algorithms to classify tweets from Twiiter page. It pulls tweets that are at EpicGames to determine the overall experience consumers are having on their platform based on 6 classifications."
                         /> 
                     <PCard 
                         link="https://github.com/RolyatVo/Inflater"
