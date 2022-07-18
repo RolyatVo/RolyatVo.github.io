17008a1c6e40499e9e3c14a603bc6bd51a63afcd
RolyatVo <taylor.vo@wsu.edu>
2022-07-18 15:28:28 -0700

    Finished Footer

--- src/componenets/Footer/Footer.js
+++ src/componenets/Footer/Footer.js
@@ -1,8 +1,19 @@
 function Footer (props) {
     return (
-        <div className="Footer fixed left-0 bottom-0 w-full h-12">
-            <div className="bg-black text-white font-bold font-6xl h-[50px] flex justify-center">
-                <div className="bg-transparent my-auto">Quick Links</div>
+        <div className="Footer fixed left-0 bottom-0 w-full h-12 flex justify-center">
+            <div className="bg-black text-white font-bold font-6xl h-[50px] flex justify-around w-full">
+                <div className="bg-transparent my-auto">Website created by Taylor Vo</div>
+                <div className="flex justify-center my-auto gap-5">Socials 
+                <a href="https://github.com/RolyatVo">
+                <i className="fa-brands fa-github"></i>
+                </a>
+                <a href="https://www.linkedin.com/in/taylorvo2022/">
+                    <i className="fa-brands fa-linkedin"></i>
+                </a>
+                <a href="mailto:taylor.c.vo@gmail.com">
+                <i className="fa-solid fa-envelope"></i>
+                </a>
+                </div>
             </div>
         </div>
     );
